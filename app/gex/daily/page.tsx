14459e4739e97530d3660cb79d6a3ce924cec0a9
kelly <systematic1@yahoo.com>
2026-08-26 20:38:01 -0400

    Started Gex Daily chart

--- app/gex/daily/page.tsx
+++ app/gex/daily/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { 
   ResponsiveContainer, 
   BarChart, 
@@ -9,26 +9,122 @@ import {
   YAxis, 
   CartesianGrid, 
   ReferenceLine,
-  Legend 
+  Legend, 
+  LabelList
 } from 'recharts';
+
+const apiUrlPrefix = "http://localhost:8080";
  
 export default function GexDaily() {
 
-    const [chartData, setChartData] = useState([
-        { strikePrice: 7750, callGex: 25, putGex: 0 },
-        { strikePrice: 7755, callGex: 31, putGex: -1 },
-        { strikePrice: 7760, callGex: 18, putGex: -2 }
-    ]);
+    const [symbols, setSymbols] = useState([]);
+    const [expirations, setExpirations] = useState([]);
+    const [snapshots, setSnapshots] = useState([]);
+    const [selectedSymbol, setSelectedSymbol] = useState(null);
+    const [selectedExpDate, setSelectedExpDate] = useState(null);
+    const [selectedSnapshot, setSelectedSnapshot] = useState(null);
+
+    const [chartData, setChartData] = useState([]);
+
+    const handleSymbolChange = (event: any) => {
+        setSelectedSymbol(event.target.value);
+    };
+
+    const handleExpDateChange = (event: any) => {
+        setSelectedExpDate(event.target.value);
+    }
+
+    const handleSnapshotChange = (event: any) => {
+        setSelectedSnapshot(event.target.value);
+    }
+
+    const getSnapshotDisplay = (item: any) => {
+        const [d, t] = item.split('T');
+        const [hms, ms] = t.split('.');
+        return hms;
+    }
+
+    const convertToChartData = (data: any[]): any[] => {
+        // Need to match Call and Put option data to align to 
+        const strikes = new Set();
+        data.forEach(item => strikes.add(item.strikePrice));
+        
+        const strikeData = [];
+        let strike, call, put;
+        strikes.forEach(sp => {
+            call = data.find(item => item.isCall() && item.strikePrice === sp);
+            put = data.find(item => item.isPut() && item.strikePrice === sp);
+
+            strike = { 
+                strikePrice: sp, 
+                callGex: call?.gex, 
+                putGex: put?.gex, 
+                absGex: Math.abs((call?.gex ?? 0) + (put?.gex ?? 0)), 
+                volume: 0, 
+                openInt: 0 
+            };
+            strikeData.push(strike);
+            strike = undefined;
+        });
+    }
   
+    useEffect(() => {
+        fetch(`${apiUrlPrefix}/Metadata/Symbols`)
+            .then(response => response.json())
+            .then(data => {
+                setSymbols(data);
+                setExpirations([]);
+            });
+    }, []);
+    useEffect(() => {
+        if (selectedSymbol) { 
+            fetch(encodeURI(`${apiUrlPrefix}/Metadata/Symbol/${selectedSymbol}/Expirations`))
+                .then(response => response.json())
+                .then(data => setExpirations(data));
+        }
+    }, [selectedSymbol]);
+    useEffect(() => {
+        if (selectedSymbol && selectedExpDate) {
+            fetch(encodeURI(`${apiUrlPrefix}/Metadata/Symbol/${selectedSymbol}/Snapshots?expiration=${selectedExpDate}`))
+                .then(response => response.json())
+                .then(data => setSnapshots(data));
+        }
+    }, [selectedExpDate]);
+    useEffect(() => { 
+        if (selectedExpDate && selectedSnapshot) {
+            fetch(encodeURI(`${apiUrlPrefix}/OptionChain/Data/BySnapshot?symbol=${selectedSymbol}&expiration=${selectedExpDate}&dateTime=${selectedSnapshot}`))
+                .then(response => response.json())
+                .then(data => setChartData(data));
+        }
+    }, [selectedSnapshot]); 
+
     return (
     <>
         <header className="grid grid-cols-3">
-            <div className="col-span-1">
-                <h2>Option Gamma Exposure - Daily</h2>
+            <div className="col-span-1 title font-bold">
+                Option Gamma Exposure - Daily
             </div>
             <div className="col-span-2 text-right">
-                [Symbol Selector] &nbsp;
-                [Expiration Selector]
+                <select value={selectedSymbol ?? ''} onChange={handleSymbolChange}>
+                    <option key="" value="">---- SYMBOL ----</option>
+                    {symbols && symbols.map(item => (
+                        <option key={item} value={item}>{item}</option> 
+                    ))}
+                </select>
+                 &nbsp;
+                <select value={selectedExpDate ?? ''} onChange={handleExpDateChange}>
+                    <option key="" value="">---- EXP DATE ----</option>
+                    {expirations && expirations.map(item => (
+                        <option key={item} value={item}>{item}</option>
+                    ))}
+                </select>
+                &nbsp;
+                <select value={selectedSnapshot ?? ''} onChange={handleSnapshotChange}>
+                    <option key="" value="">-- SNAPSHOT TIME --</option>
+                    {snapshots && snapshots.map(item => (
+                        <option key={item} value={item}>{getSnapshotDisplay(item)}</option>
+                    ))}
+                </select>
             </div>
         </header>
         <div className="chart-container">
@@ -37,21 +133,13 @@ export default function GexDaily() {
                     <CartesianGrid strokeDasharray="3, 3" vertical={false} />
                     <XAxis 
                         dataKey="strikePrice"
-                        tickLine={false}
-                        dy={10}
                     />
                     <YAxis />
                     <Legend />
                     <ReferenceLine y={0} stroke="#999" strokeWidth={1.5} />
                     <Bar 
-                        dataKey="callGex" 
+                        dataKey="gex"
                         fill="#10b981" 
-                        radius={[4, 4, 0, 0]} 
-                    />
-                    <Bar 
-                        dataKey="putGex" 
-                        fill="#ef4444" 
-                        radius={[0, 0, 4, 4]} 
                     />
                 </BarChart>
             </ResponsiveContainer>
